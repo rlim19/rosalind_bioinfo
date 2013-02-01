21fcf198afa617dffd6023e7f2912f9f9f273f05
rickylim19 <rickylim19@gmail.com>
2013-02-01 19:20:41 +0100

    make a py script to run this c code

---
+++ getCommonSubstring.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#http://www.daimi.au.dk/~mailund/suffix_tree.html
+from suffix_tree import GeneralisedSuffixTree
+#https://hkn.eecs.berkeley.edu/~dyoo/python/suffix_trees/
+
+gc.disable()
+stree = GeneralisedSuffixTree(['mississippi','sippissi'])
+
+gc.enable()
